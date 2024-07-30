677b77e0706cb8510d1fb335c482d3d9c5087529
AmithBV0606 <amithrao0606@gmail.com>
2024-07-30 11:46:33 +0530

    Studied in-depth about Fetch API and it's working.

---
+++ Fetch-API/script.js
@@ -0,0 +1,7 @@
+// Fetch API
+
+// The Fetch API provides an interface for fetching resources (including across the network). It is a more powerful and flexible replacement for "XMLHttpRequest".
+
+//  The global "fetch()" method starts the process of fetching a resource from the network, returning a promise which is fulfilled once the response is available.
+
+// The promise resolves to the Response object representing the response to your request.
